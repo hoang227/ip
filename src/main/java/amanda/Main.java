83486e61904eb6221f642baa6e60374e8dfff66f
hoang227 <hoangg227@gmail.com>
2022-09-03 15:45:39 +0800

    added more javadoc comments

--- src/main/java/amanda/Main.java
+++ src/main/java/amanda/Main.java
@@ -1,5 +1,8 @@
 package main.java.amanda;
 
+/**
+ * Main class that runs the program
+ */
 public class Main {
     public static void main(String[] args) {
         Amanda amanda = new Amanda("./src/main/java/data/amanda.txt");
